321a83e246f76504f4cc3a6418d525fd8e985217
Kamil Sachryn <k.sachryn@gmail.com>
2024-04-21 13:46:03 -0400

    p>6

--- LeetCode50/Problems.cs
+++ LeetCode50/Problems.cs
@@ -1,11 +1,14 @@
-﻿using static System.Net.Mime.MediaTypeNames;
+﻿using System.Collections;
+using static System.Net.Mime.MediaTypeNames;
 
 internal class Problems
 {
     public void currentProblem()
     {
-        string s = "qrsvbspk";
-        Console.WriteLine(LengthOfLongestSubstring(s));
+        int[] nums1 = { 1, 2 };
+        int[] nums2 = { 3, 4 };
+        
+        Console.WriteLine(Reverse(123));
     }
 
 
@@ -151,5 +154,237 @@ internal class Problems
 
         return best;
 
+    }
+
+    //#4. Median of Two Sorted Arrays
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        //read heads
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        int[] nums3 = new int[nums1.Length + nums2.Length];
+
+
+        //loop read heads
+        while (i < nums1.Length || j < nums2.Length)
+        {
+            //if both read heads are reading a value
+            if(i < nums1.Length && j < nums2.Length)
+            {
+                if(nums1[i] < nums2[j])
+                {
+                    nums3[k++] = nums1[i++];
+                }
+                else
+                {
+                    nums3[k++] = nums2[j++];
+                }
+            }
+            else if(i >= nums1.Length)
+            {
+                nums3[k++] = nums2[j++];
+            }
+            else if(j >= nums2.Length)
+            {
+                nums3[k++] = nums1[i++];
+            }
+        }
+
+        double sum = 0;
+        if (nums3.Length % 2 == 0)
+        {
+            int x = nums3[(nums3.Length - 1)/ 2];
+            int y = nums3[(nums3.Length - 1)/ 2 + 1];
+            sum = x + y;
+            sum /= 2;
+        }
+        else
+        {
+            sum = nums3[nums3.Length / 2];
+        }
+
+
+
+        return sum;
+    }
+
+    //#5. Longest Palindromic Substring
+    public string LongestPalindrome(string s)
+    {
+        int i = 0;
+        int j = 1;
+        string longest = "";
+
+        while(i < s.Length )
+        {
+            string curr = s.Substring(i, j);
+            Console.WriteLine("looking at : " + curr);
+
+            bool pal = isPalindrome(curr);
+            if(pal)
+            {
+                if(curr.Length > longest.Length)
+                {
+                    longest = curr;
+                }
+            }
+
+            j++;
+            if(j + i > s.Length)
+            {
+                j = 1;
+                i++;
+            }
+
+        }
+
+        return longest;
+    }
+    bool isPalindrome(string s)
+    {
+        int i = 0;
+        int j = s.Length - 1;
+        while (j >= i)
+        {
+            if (s[j--] != s[i++])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    //#6. Zigzag Conversion
+    public string Convert(string s, int numRows)
+    {
+        char[,] chars = new char[s.Length * numRows,numRows];
+
+        int i = 0;
+        bool dirDown = true;
+
+
+        i = 0;
+
+
+        int x = 0;
+        int y = 0;
+        string output = s;
+        while (i < s.Length)
+        {
+
+            y = Math.Min(numRows - 1, y);
+            y = Math.Max(0, y);
+
+            if (y == 0)
+            {
+                dirDown = true;
+            }
+            else if(y == numRows - 1)
+            {
+                dirDown = false;
+            }
+
+            int mod = i % numRows;
+
+            Console.WriteLine(String.Format("X: {0},\t Y: {1},\t dirDown:{2},\t i: {3},\tmod: {5},\t currChar: {4}", x, y, dirDown ? "down" : "up", i, s[i], mod));
+
+
+
+            chars[x, y] = s[i];
+
+
+
+            if (dirDown && y != numRows - 1)
+            {
+                y++;
+            }
+            else if(dirDown && y == numRows - 1)
+            {
+                y--;
+                x++;
+            }
+            else if(!dirDown && y == 0)
+            {
+                y++;
+                x++;
+            }
+            else if(!dirDown && y != 0)
+            {
+                y--;
+                x++;
+                
+            }
+
+
+
+
+
+            i++;
+            
+        }
+
+        
+
+        Print2DCharArray(chars);
+        for (int ii = 0; ii < chars.GetLength(1); ii++)
+        {
+            for (int jj = 0; jj < chars.GetLength(0); jj++)
+            {
+                char c = chars[jj, ii];
+                if(c != '\0')
+                {
+                    output += c;
+                }
+
+            }
+        }
+
+
+
+        return output;
+    }
+
+    public void Print2DCharArray(char[,] arr)
+    {
+        for(int i = 0; i < arr.GetLength(1); i++)
+        {
+            for(int j = 0; j < arr.GetLength(0);j++)
+            {
+                char c = arr[j, i];
+                if(c == '\0')
+                {
+                    c = '_';
+                }
+
+                Console.Write(c);
+               
+            }
+            Console.WriteLine();
+        }
+    }
+
+    //#7. Reverse Integer
+    public int Reverse(int x)
+    {
+        int len = x.ToString().Length;
+        bool neg = x < 0 ? true : false;
+        int i = 10^ len;
+
+        int output = 0;
+        Console.WriteLine("Input: " + x);
+        while (x > 0)
+        {
+            int last = x % 10;
+            x = x / 10;
+            output += last * i;
+            i /= 10;
+            Console.Write(last);
+        }
+        Console.WriteLine();
+
+
+        return output * (neg ? -1 : 1);
     }
 }
