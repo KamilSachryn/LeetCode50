1a7e51726747374d3afd0b0e3f294f914553168e
Kamil Sachryn <k.sachryn@gmail.com>
2024-04-22 11:34:49 -0400

    p7 8

--- LeetCode50/Problems.cs
+++ LeetCode50/Problems.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Numerics;
 using static System.Net.Mime.MediaTypeNames;
 
 internal class Problems
@@ -8,7 +9,7 @@ internal class Problems
         int[] nums1 = { 1, 2 };
         int[] nums2 = { 3, 4 };
         
-        Console.WriteLine(Reverse(123));
+        Console.WriteLine(MyAtoi("2147483648"));
     }
 
 
@@ -368,23 +369,146 @@ internal class Problems
     //#7. Reverse Integer
     public int Reverse(int x)
     {
-        int len = x.ToString().Length;
-        bool neg = x < 0 ? true : false;
-        int i = 10^ len;
-
+        //bool neg = x < 0 ? true : false;
         int output = 0;
-        Console.WriteLine("Input: " + x);
-        while (x > 0)
+
+        while(x != 0)
         {
             int last = x % 10;
-            x = x / 10;
-            output += last * i;
-            i /= 10;
-            Console.Write(last);
+            int prevOutput = output;
+
+
+            output = output * 10 + last;
+
+            if((output - last) / 10 != prevOutput)
+            {
+                return 0;
+            }
+
+            x /= 10;
         }
-        Console.WriteLine();
+
+        return output;// * (neg ? -1 : 1);
+    }
+
+    //#8. String to Integer (atoi)
+    public int MyAtoi(string s)
+    {
+        Console.WriteLine("Start string: " + s);
+        int output = 0;
+        s = s.Trim();
+        if(s.Length == 0)
+        {
+            return 0;
+        }
+        bool neg = s[0] == '-' ? true : false;
+        Console.WriteLine("neg: " + neg);
+
+        if (neg || s[0] == '+')
+        {
+            s = s.Substring(1, s.Length - 1);
+        }
+        string temp = "";
+
+        Console.WriteLine("trimmed string: " + s);
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (char.IsDigit(s[i]))
+            {
+                temp += s[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+        Console.WriteLine("numbers only string: " + temp);
+
+
+        foreach (char c in temp)
+        {
+            Console.WriteLine("output: " + output);
+            int curr = (c - '0');
+            int prevOutput = output;
+            output = output * 10 + curr;
+            Console.WriteLine("newOutput: " + output + " newOutputDiv: " + (output - curr) / 10 + ", prevOutput: " + prevOutput);
+            if ((output - curr) / 10 != prevOutput || output < 0)
+            {
+                Console.WriteLine("overflow");
+                if(neg)
+                {
+                    return Int32.MinValue;
+                }
+                else
+                {
+                    return Int32.MaxValue;
+                }
+            }
+        }
+        
+        
 
 
         return output * (neg ? -1 : 1);
+
+        /*
+         *Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+        The algorithm for myAtoi(string s) is as follows:
+
+        Read in and ignore any leading whitespace.
+        Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+        Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+        Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+        If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+        Return the integer as the final result.
+        Note:
+
+        Only the space character ' ' is considered a whitespace character.
+        Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+ 
+
+        Example 1:
+
+        Input: s = "42"
+        Output: 42
+        Explanation: The underlined characters are what is read in, the caret is the current reader position.
+        Step 1: "42" (no characters read because there is no leading whitespace)
+                 ^
+        Step 2: "42" (no characters read because there is neither a '-' nor '+')
+                 ^
+        Step 3: "42" ("42" is read in)
+                   ^
+        The parsed integer is 42.
+        Since 42 is in the range [-231, 231 - 1], the final result is 42.
+        Example 2:
+
+        Input: s = "   -42"
+        Output: -42
+        Explanation:
+        Step 1: "   -42" (leading whitespace is read and ignored)
+                    ^
+        Step 2: "   -42" ('-' is read, so the result should be negative)
+                     ^
+        Step 3: "   -42" ("42" is read in)
+                       ^
+        The parsed integer is -42.
+        Since -42 is in the range [-231, 231 - 1], the final result is -42.
+        Example 3:
+
+        Input: s = "4193 with words"
+        Output: 4193
+        Explanation:
+        Step 1: "4193 with words" (no characters read because there is no leading whitespace)
+                 ^
+        Step 2: "4193 with words" (no characters read because there is neither a '-' nor '+')
+                 ^
+        Step 3: "4193 with words" ("4193" is read in; reading stops because the next character is a non-digit)
+                     ^
+        The parsed integer is 4193.
+        Since 4193 is in the range [-231, 231 - 1], the final result is 4193.
+ 
+         */
+
     }
 }
