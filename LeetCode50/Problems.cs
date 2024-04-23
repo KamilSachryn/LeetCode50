36a44e62573a73675eefde75515d8779ef78592c
Kamil Sachryn <k.sachryn@gmail.com>
2024-04-23 07:31:43 -0400

    11

--- LeetCode50/Problems.cs
+++ LeetCode50/Problems.cs
@@ -6,10 +6,8 @@ internal class Problems
 {
     public void currentProblem()
     {
-        int[] nums1 = { 1, 2 };
-        int[] nums2 = { 3, 4 };
-        
-        Console.WriteLine(MyAtoi("2147483648"));
+        Console.WriteLine(MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+
     }
 
 
@@ -446,69 +444,75 @@ internal class Problems
             }
         }
         
-        
-
-
         return output * (neg ? -1 : 1);
 
-        /*
-         *Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+    }
 
-        The algorithm for myAtoi(string s) is as follows:
 
-        Read in and ignore any leading whitespace.
-        Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
-        Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
-        Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
-        If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
-        Return the integer as the final result.
-        Note:
 
-        Only the space character ' ' is considered a whitespace character.
-        Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
- 
+    //118. Pascal's Triangle
+    public IList<IList<int>> Generate(int numRows)    
+    {
+        List<IList<int>> triangle = new List<IList<int>>();
+        if (numRows == 0)
+        {
+            return triangle;
+        }
 
-        Example 1:
+        triangle.Add(new List<int>() { 1 });
 
-        Input: s = "42"
-        Output: 42
-        Explanation: The underlined characters are what is read in, the caret is the current reader position.
-        Step 1: "42" (no characters read because there is no leading whitespace)
-                 ^
-        Step 2: "42" (no characters read because there is neither a '-' nor '+')
-                 ^
-        Step 3: "42" ("42" is read in)
-                   ^
-        The parsed integer is 42.
-        Since 42 is in the range [-231, 231 - 1], the final result is 42.
-        Example 2:
+        for(int i = 1; i < numRows; i++)
+        {
+            IList<int> previousList = triangle[i - 1];
+            List<int> newList = new List<int>() { 1 };
 
-        Input: s = "   -42"
-        Output: -42
-        Explanation:
-        Step 1: "   -42" (leading whitespace is read and ignored)
-                    ^
-        Step 2: "   -42" ('-' is read, so the result should be negative)
-                     ^
-        Step 3: "   -42" ("42" is read in)
-                       ^
-        The parsed integer is -42.
-        Since -42 is in the range [-231, 231 - 1], the final result is -42.
-        Example 3:
+            for(int j = 1; j < i; j++)
+            {
+                newList.Add(previousList[j-1] + previousList[j]);
 
-        Input: s = "4193 with words"
-        Output: 4193
-        Explanation:
-        Step 1: "4193 with words" (no characters read because there is no leading whitespace)
-                 ^
-        Step 2: "4193 with words" (no characters read because there is neither a '-' nor '+')
-                 ^
-        Step 3: "4193 with words" ("4193" is read in; reading stops because the next character is a non-digit)
-                     ^
-        The parsed integer is 4193.
-        Since 4193 is in the range [-231, 231 - 1], the final result is 4193.
- 
-         */
+            }
 
+            newList.Add(1);
+            triangle.Add(newList);
+
+        }
+
+        return triangle;
+    }
+
+    //11. Container With Most Water
+    public int MaxArea(int[] height)
+    {
+        int output = 0;
+
+        int backHead = 0;
+        int forwardHead = height.Length - 1;
+
+        while(backHead < forwardHead)
+        {
+            
+            int i = height[backHead];
+            int j = height[forwardHead];
+            int dist = forwardHead - backHead;
+            int area = dist * Math.Min(i, j);
+
+            Console.WriteLine(String.Format("back: {0}, forward: {1}, i: {2}, j: {3}, dist: {4}, area: {5}", backHead, forwardHead, i, j, dist, area));
+            if (area > output)
+            {
+                output = area;
+            }
+
+            if (i < j)
+                backHead++;
+            else
+                forwardHead--;
+
+
+        }
+
+
+
+
+        return output;
     }
 }
