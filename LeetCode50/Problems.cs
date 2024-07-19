a81085133a302aca3e619ded63875e99d7b011ba
Kamil Sachryn <k.sachryn@gmail.com>
2024-07-19 14:24:36 -0400

    263

--- LeetCode50/Problems.cs
+++ LeetCode50/Problems.cs
@@ -23,11 +23,51 @@ internal class Problems
 
         DuplicateZeros(arr);
 
-
+        IsUgly(6);
 
 
     }
 
+
+    //263. Ugly Number
+    public bool IsUgly(int n)
+    {
+        if (n <= 0)
+            return false;
+        if (n == 1)
+            return true;
+
+
+        while(n >= 1)
+        {
+
+            if(n == 1)
+            {
+                return true;
+            }
+            else if(n % 2 == 0)
+            {
+                n = n / 2;
+            }
+            else if(n % 3 == 0)
+            {
+                n = n / 3;
+            }
+            else if(n % 5 == 0)
+            {
+                n = n / 5;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return false;
+       
+
+
+    }
 
     //1089. Duplicate Zeros
     public void DuplicateZeros(int[] arr)
